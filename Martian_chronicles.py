882e1e2f480206482cb16c0c09c64f437c4a9e6a
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-02-08 10:43:16 +0530

    fix: skipping over images

--- Martian_chronicles.py
+++ Martian_chronicles.py
@@ -1,24 +1,24 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget ,QHBoxLayout, QGridLayout ,QButtonGroup,QLineEdit
-from PySide6.QtGui import QPixmap
 from buttons import *
-from image import *
+
 
 class MainWindow(QMainWindow):
     
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
+    def __init__(self,parent=None):
+        super().__init__(parent)
         mainlayout = QHBoxLayout()
         buttonlayout = QVBoxLayout()
-        lineedit = LineEdit()
-        button1 = Button1(mainlayout)
-        button2 = Button2(mainlayout)
+        imagelayout = QVBoxLayout()
+        stacked_widget = QStackedWidget()
+        self.line_edit = QLineEdit(self)
+        button1 = Button1(stacked_widget)
+        button2 = Button2(stacked_widget)
         button3 = Button3(self)
-        button4 = Fetchbutton(lineedit)
+        button4 = Fetchbutton(self.line_edit,mainlayout,imagelayout,stacked_widget)
         rb1 = Curiosity()
         rb2 = Opportunity()
         rb3 = Spirit()
-        
         buttonlayout.addWidget(button1)
         buttonlayout.addWidget(button2)
         buttonlayout.addWidget(button3)
@@ -26,15 +26,19 @@ class MainWindow(QMainWindow):
         buttonlayout.addWidget(rb1)
         buttonlayout.addWidget(rb2)
         buttonlayout.addWidget(rb3)
-        buttonlayout.addWidget(lineedit)
+        buttonlayout.addWidget(self.line_edit)
         mainlayout.addLayout(buttonlayout)
         central_widget = QWidget()
         central_widget.setLayout(mainlayout)
         self.setCentralWidget(central_widget)
         self.setGeometry(100, 100, 600, 400)
+    def taketext(self):
+        global textval
+        textval = self.line_edit.text()
+        print(textval)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
         
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = QApplication(sys.argv)                                
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
--- buttons.py
+++ buttons.py
@@ -1,18 +1,18 @@
-from PySide6.QtWidgets import  QPushButton,QVBoxLayout,QRadioButton,QLineEdit
+from PySide6.QtWidgets import  QPushButton,QVBoxLayout,QRadioButton,QLineEdit,QStackedWidget,QWidget,QLabel
+from PySide6.QtGui import QPixmap
 import requests,os,json
-from image import *
-from Martian_chronicles import *
+import Martian_chronicles
 marskey = os.getenv("marskey")
 rovername = ""
-earth_date = "earth_date=2015-6-3&"
 count = None
+earth_date = ""
 class Button1(QPushButton):
-    def __init__(self, mainlayout, *args, **kwargs):
-        super().__init__(*args,**kwargs)
+    def __init__(self, stacked_widget):
+        super().__init__()
         self.setText("Next Image")
         self.clicked.connect(self.showMessage)
         self.setFixedSize(120,50)
-        self.mainlayout = mainlayout
+        self.stacked_widget = stacked_widget
     def showMessage(self):
         global count
         if count == None:
@@ -23,26 +23,27 @@ class Button1(QPushButton):
             else:
                 count = 0
         print(count)
-        if self.mainlayout.count() == 1:
-            imagelayout = QVBoxLayout()
-            image = Image(f"./{count}.png")     
-            imagelayout.addWidget(image)
-            self.mainlayout.addLayout(imagelayout)
-        else:
-            im = self.mainlayout.takeAt(1)
-            try:
-                im.pop()
-            except AttributeError:
-                pass
+        self.stacked_widget.setCurrentIndex(count)
+        # if self.mainlayout.count() == 1:
+        #     imagelayout = QVBoxLayout()
+        #     image = Image(f"./{count}.png")     
+        #     imagelayout.addWidget(image)
+        #     self.mainlayout.addLayout(imagelayout)
+        # else:
+        #     im = self.mainlayout.takeAt(1)
+        #     try:
+        #         im.pop()
+        #     except AttributeError:
+        #         pass
 
 
 class Button2(QPushButton):
-    def __init__(self, mainlayout, *args, **kwargs):
-        super().__init__(*args,**kwargs)
+    def __init__(self,stacked_widget):
+        super().__init__()
         self.setText("Previous Image")
         self.clicked.connect(self.showMessage)
         self.setFixedSize(120,50)
-        self.mainlayout = mainlayout
+        self.stacked_widget = stacked_widget
     def showMessage(self):
         global count
         if count == None:
@@ -53,17 +54,8 @@ class Button2(QPushButton):
             else:
                 count = 24
         print(count)
-        if self.mainlayout.count() == 1:
-            imagelayout = QVBoxLayout()
-            image = Image(f"./{count}.png")     
-            imagelayout.addWidget(image)
-            self.mainlayout.addLayout(imagelayout)
-        else:
-            im = self.mainlayout.takeAt(1)
-            try:
-                im.pop()
-            except AttributeError:
-                pass
+        self.stacked_widget.setCurrentIndex(count)
+        
 
 class Button3(QPushButton):
 
@@ -77,28 +69,42 @@ class Button3(QPushButton):
         print("Button 3 was clicked")
 
 class Fetchbutton(QPushButton):
-    def __init__ (self):
+    def __init__ (self,linedit,mainlayout,imagelayout,stacked_widget):
         super().__init__()
         self.setText("Fetch Image")
         self.setFixedSize(120,50)
-
+        self.line_edit = linedit
+        self.imagelayout = imagelayout
+        self.stacked_widget = stacked_widget
+        self.mainlayout = mainlayout
         self.clicked.connect(self.fetchdata)
+        
     
     def fetchdata(self):
-        print(self.lineedit.text())
-        print(f"https://api.nasa.gov/mars-photos/api/v1/rovers/{rovername}/latest_photos?{earth_date}api_key={marskey}")
-        # json_data = requests.get(f"https://api.nasa.gov/mars-photos/api/v1/rovers/{rovername}/latest_photos?{earth_date}api_key={marskey}").json()
+        # global earth_date
+        # earth_date = "earth_date="+self.line_edit.text()+"&"
+        # print(f"https://api.nasa.gov/mars-photos/api/v1/rovers/{rovername}/photos?{earth_date}api_key={marskey}")
+        # json_data = requests.get(f"https://api.nasa.gov/mars-photos/api/v1/rovers/{rovername}/photos?{earth_date}api_key={marskey}").json()
         # print(json.dumps(json_data,indent= 3))
         # urlist = []
         # for i in range(25):
         #     try:
-        #         urlist.append(json_data["latest_photos"][i]["img_src"])
+        #         urlist.append(json_data["photos"][i]["img_src"])
         #     except IndexError:
         #         break
         # for index,item in enumerate(urlist):
         #     response = requests.get(item,allow_redirects=True)
         #     if response.status_code:
         #         open(f"{index}.png",'wb').write(response.content)
+
+        for i in range(25):
+            label = QLabel()
+            pixmap = QPixmap(f"{i}.png").scaled(400,400)
+            label.setPixmap(pixmap)
+            self.stacked_widget.addWidget(label)
+        self.stacked_widget.setCurrentIndex(0)
+        self.imagelayout.addWidget(self.stacked_widget)
+        self.mainlayout.addLayout(self.imagelayout)
 
 class Curiosity(QWidget):
     def __init__(self):
@@ -130,10 +136,10 @@ class Spirit(QWidget):
         self.radio = QRadioButton("Spirit", self)
         self.radio.toggled.connect(self.ischecked)   
     def ischecked(self, checked):
-        global rovername
         if checked:
-            rovername = 'Spirit'
+            self.rovername = 'Spirit'
             
-class LineEdit(QWidget):
-    def __init__(self):
+class LineEdit(QLineEdit):
+    def __init__(self,parent=None):
         super().__init__()        
+        self.setFixedSize(120,25)
