9ab2130c06cb617b04d73b3e8fddce7cbd79ffa9
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-02-09 18:55:10 +0530

    progress: Mutlti threading implemented

--- buttons.py
+++ buttons.py
@@ -1,6 +1,6 @@
-from PySide6.QtWidgets import  QPushButton,QVBoxLayout,QRadioButton,QLineEdit,QWidget,QLabel
+from PySide6.QtWidgets import  QPushButton,QVBoxLayout,QRadioButton,QLineEdit,QWidget,QLabel,QComboBox
 from PySide6.QtGui import QPixmap
-from PySide6.QtCore import QThread
+import threading
 import requests,os,json,ezgmail
 import Martian_chronicles
 os.chdir(r'/home/wreck/Desktop/Projects/Martian-Chronicles/')
@@ -8,12 +8,14 @@ marskey = os.getenv("marskey")
 rovername = ""
 count = None
 earth_date = ""
+
+
 class Button1(QPushButton):
     def __init__(self, stacked_widget):
         super().__init__()
         self.setText("Next Image")
         self.clicked.connect(self.showMessage)
-        self.setFixedSize(180,50)
+        self.setFixedSize(200,50)
         self.stacked_widget = stacked_widget
     def showMessage(self):
         global count
@@ -33,7 +35,7 @@ class Button2(QPushButton):
         super().__init__()
         self.setText("Previous Image")
         self.clicked.connect(self.showMessage)
-        self.setFixedSize(180,50)
+        self.setFixedSize(200,50)
         self.stacked_widget = stacked_widget
     def showMessage(self):
         global count
@@ -53,57 +55,79 @@ class Button3(QPushButton):
     def __init__(self, linedit):
         super().__init__()
         self.setText("Send Email")
-        self.setFixedSize(180,50)
+        self.setFixedSize(200,50)
         self.clicked.connect(self.showMessage)
         self.linedit = linedit
         
     def showMessage(self):
-        mail = self.linedit.text()
-        ezgmail.send(mail,'lmao','idk',['0.png'])
-        print("sent email to ",mail)
+        mail = self.linedit.text().split(',')
+        for i in mail:
+            ezgmail.send(i,'lmao','idk',['0.png'])
+            print("sent email to ",i)
 
 class Fetchbutton(QPushButton):
     def __init__ (self,mainlayout,imagelayout,stacked_widget,calendar):
         super().__init__()
-        self.setText("Fetch Image")
-        self.setFixedSize(180,50)
+        self.setText("Fetch")
+        self.setFixedSize(95,50)
         self.calender = calendar
         self.imagelayout = imagelayout
         self.stacked_widget = stacked_widget
         self.mainlayout = mainlayout
-        self.clicked.connect(self.fetchdata)
+        self.clicked.connect(self.threading)
         
     
     def fetchdata(self):
         earth_date  = self.calender.selectedDate().toString()
         earth_date = earth_date.split()
         earth_date.pop(0)
-        monthdict = {"Jan":"1","Feb":"2","Mar":"3","Apr":"4","May":"5","Jun":"6","Jul":"7","Aug":"8","Sep":"9","Oct":"10","Now":"11","Dec":"12"}
+        monthdict = {"Jan":"1","Feb":"2","Mar":"3","Apr":"4","May":"5","Jun":"6","Jul":"7","Aug":"8","Sep":"9","Oct":"10","Nov":"11","Dec":"12"}
         earth_date[0] = monthdict[earth_date[0]]
-        print(earth_date)
-        # earth_date = "earth_date="+self.line_edit.text()+"&"
-        # print(f"https://api.nasa.gov/mars-photos/api/v1/rovers/{rovername}/photos?{earth_date}api_key={marskey}")
-        # json_data = requests.get(f"https://api.nasa.gov/mars-photos/api/v1/rovers/{rovername}/photos?{earth_date}api_key={marskey}").json()
-        # print(json.dumps(json_data,indent= 3))
-        # urlist = []
-        # for i in range(25):
-        #     try:
-        #         urlist.append(json_data["photos"][i]["img_src"])
-        #     except IndexError:
-        #         break
-        # for index,item in enumerate(urlist):
-        #     response = requests.get(item,allow_redirects=True)
-        #     if response.status_code:
-        #         open(f"{index}.png",'wb').write(response.content)
+        month = earth_date[0]
+        day = earth_date[1]
+        year = earth_date[2]
+        earth_date = year + "-" + month + "-" + day
+        print(f"https://api.nasa.gov/mars-photos/api/v1/rovers/{rovername}/photos?earth_date={earth_date}&api_key={marskey}")
+        json_data = requests.get(f"https://api.nasa.gov/mars-photos/api/v1/rovers/{rovername}/photos?earth_date={earth_date}&api_key={marskey}").json()
+        print(json.dumps(json_data,indent= 3))
+        urlist = []
+        for i in range(25):
+            try:
+                urlist.append(json_data["photos"][i]["img_src"])
+            except IndexError:
+                break
+        for index,item in enumerate(urlist):
+            response = requests.get(item,allow_redirects=True)
+            if response.status_code:
+                open(f"{index}.png",'wb').write(response.content)
 
+
+
+    def threading(self):
+        thread = threading.Thread(target=self.fetchdata)
+        thread.start()
+
+
+
+class ShowButton(QPushButton):
+    def __init__ (self,stacked_widget,imagelayout,mainlayout):
+        super().__init__()
+        self.setText("Show")
+        self.setFixedSize(95,50)
+        self.clicked.connect(self.showImage)
+        self.stacked_widget = stacked_widget
+        self.imagelayout = imagelayout
+        self.mainlayout = mainlayout
+    def showImage(self):
         for i in range(25):
             label = QLabel()
-            pixmap = QPixmap(f"{i}.png").scaled(600,600)
+            pixmap = QPixmap(f"{i}.png").scaled(500,480)
             label.setPixmap(pixmap)
             self.stacked_widget.addWidget(label)
         self.stacked_widget.setCurrentIndex(0)
         self.imagelayout.addWidget(self.stacked_widget)
-        self.mainlayout.addLayout(self.imagelayout)
+        self.mainlayout.addLayout(self.imagelayout)       
+
 
 class Curiosity(QWidget):
     def __init__(self):
@@ -141,6 +165,20 @@ class Spirit(QWidget):
             rovername = 'Spirit'
             print(rovername)
             
+class Roverlist(QComboBox):
+    def __init__(self):
+        super().__init__()
+        self.addItems(["Curiosity","Opportunity","Spirit"])
+        self.currentIndexChanged.connect(self.indexchanged)
+
+    def indexchanged(self,index):
+        global rovername
+        if index == 0:
+            rovername = "curiosity"
+        if index == 1:
+            rovername = "opportunity"
+        if index == 2:
+            rovername = "spirit"
 
 # class CloneThread(QThread):
 #     signal = Signal('PyQt_PyObject')
